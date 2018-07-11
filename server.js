1d546198d73d577d750c56a903c53b0ff70ae7ad
Sheraz Arshad <sheraz1392@gmail.com>
2018-07-11 14:40:18 +0500

    change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const Config = require("./Config");
 
 app.use(bodyParser.json());
 
-app.get("/OKM/:toAddress", async (req, res) => {
+app.get("/OKM/:toAddress", (req, res) => {
     const to_address = req.params.toAddress;
 
     const w = new WalletProvider(Config.privKey.trim(), "https://ropsten.infura.io/QWMgExFuGzhpu2jUr6Pq")
@@ -19,25 +19,27 @@ app.get("/OKM/:toAddress", async (req, res) => {
 
     const contract = new web3.eth.Contract(ABI,"0x41891953f0fc6f75b501fedd916bc9bc0fb7c235");
 
-    const decimals = await contract.methods.decimals().call();
+    contract.methods.decimals().call().then((decimals) => {
+        web3.eth.getGasPrice( (err, gasPrice) => {
+            console.log("Gas Price is: ", gasPrice);
+    
+            web3.eth.getAccounts( (err, accounts) => {
+                if (err) {
+                    console.log("errr")
+                    throw err;
+                }
+                contract.methods.transfer(to_address.trim(), 100 * (10 ** decimals)).send({
+                    from: accounts[0],
+                    gas: '2100000',
+                    gasPrice
+                }).once('transactionHash', function(hash){ 
+                    res.send({hash, receiving_address:to_address})
+                 })
+            });
+        })
+    });
 
-    web3.eth.getGasPrice(async (err, gasPrice) => {
-        console.log("Gas Price is: ", gasPrice);
-
-        web3.eth.getAccounts(async (err, accounts) => {
-            if (err) {
-                console.log("errr")
-                throw err;
-            }
-            contract.methods.transfer(to_address.trim(), 100 * (10 ** decimals)).send({
-                from: accounts[0],
-                gas: '2100000',
-                gasPrice
-            }).once('transactionHash', function(hash){ 
-                res.send({hash, receiving_address:to_address})
-             })
-        });
-    })
+  
 
 
 
